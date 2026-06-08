64b96d586a2e246bfc1671acca5ace26c99ae751
Vishal05it <vishal.tiwari0510@gmail.com>
2026-06-08 19:49:03 +0530

    Hide handshake

--- socketAuth.js
+++ socketAuth.js
@@ -5,9 +5,9 @@ const userModel = require("./user.model");
 module.exports = async (socket, next) => {
     console.log(`Socket : `, socket);
     const cookieHeader = socket.handshake.headers.cookie;
-    console.log(
-        "Handshake headers : ", socket.handshake.headers.cookie
-    );
+    // console.log(
+    //     "Handshake headers : ", socket.handshake.headers.cookie
+    // );
     console.log("Cookies : ", socket.cookies)
     if (!cookieHeader) {
         console.log("Authentication required!");
